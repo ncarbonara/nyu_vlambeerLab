314730b1004567efede1fe0b6beb04a39e94966c
Unknown <ncarbonara235@gmail.com>
2017-10-30 01:44:53 -0400

    Commented Code & Restricted Tile Count to Exactly 500
    Now the code can't generate an extra few tiles above the limit just because it's in the middle of generating a hall

--- Assets/scripts/Pathmaker.cs
+++ Assets/scripts/Pathmaker.cs
@@ -9,29 +9,41 @@ using UnityEngine;
 // STEP 1: ======================================================================================
 // put this script on a Sphere... it will move around, and drop a path of floor tiles behind it
 
-public class Pathmaker : MonoBehaviour {
+public class Pathmaker : MonoBehaviour
+{
 
-// STEP 2: ============================================================================================
-// translate the pseudocode below
+	// STEP 2: ============================================================================================
+	// translate the pseudocode below
 
-//	DECLARE CLASS MEMBER VARIABLES:
-//	Declare a private integer called counter that starts at 0; 		// counter var will track how many floor tiles I've instantiated
-//	Declare a public Transform called floorPrefab, assign the prefab in inspector;
-//	Declare a public Transform called pathmakerSpherePrefab, assign the prefab in inspector; 		// you'll have to make a "pathmakerSphere" prefab later
+	//	DECLARE CLASS MEMBER VARIABLES:
+	//	Declare a private integer called counter that starts at 0; 		// counter var will track how many floor tiles I've instantiated
+	//	Declare a public Transform called floorPrefab, assign the prefab in inspector;
+	//	Declare a public Transform called pathmakerSpherePrefab, assign the prefab in inspector; 		// you'll have to make a "pathmakerSphere" prefab later
 
-	public static int globalTileCount = 0;
 
-	int counter = 0;
+	//Counts the number of total tiles in the scene across all instances of the pathmaker sphere
+	public static int counter = 0;
+
+	//The floor tile prefab
 	public GameObject floorPrefab;
+
+	//The prefab for the object that creates additional floor tiles
 	public GameObject pathmakerSpherePrefab;
+
+	//An RGB value for the color paths can have
 	Vector3 pathColor;
 
-	void Start() {
-		pathColor = new Vector3 (Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+	void Start ()
+	{
+
+		//Randomizes the path color at the start
+		pathColor = new Vector3 (Random.Range (0, 256), Random.Range (0, 256), Random.Range (0, 256));
+		//pathColor = Random.ColorHSV();
 	}
 
-	void Update () {
-//	If counter is less than 50, then:
+	void Update ()
+	{
+//			If counter is less than 50, then:
 //			Generate a random number from 0.0f to 1.0f;
 //			If random number is less than 0.25f, then rotate myself 90 degrees;
 //				... Else if number is 0.25f-0.5f, then rotate myself -90 degrees;
@@ -44,46 +56,62 @@ public class Pathmaker : MonoBehaviour {
 //	Else:
 //			Destroy my game object; 		// self destruct if I've made enough tiles already
 
-		if (counter < 50) {
+		if (counter < 500) {
+
+			//Creates a random value to help determine what the pathmaker should do next
 			float randVal = Random.Range (0f, 1f);
 
+			//Turns the pathmaker sphere in one direction
 			if (randVal <= .25f) {
 				transform.Rotate (new Vector3 (0, 45, 0));
+
+			//Turns it in the other direction
 			} else if (randVal > .25f
 			           && randVal <= .5f) {
 				transform.Rotate (new Vector3 (0, -45, 0));
+
+			//Creates a new pathmaker sphere 
 			} else if (randVal > .9f
 			           && randVal <= 1f) {
+
 				GameObject pathmakerSphereClone = (GameObject)Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.Euler (0f, 0f, 0f));
-				pathColor = new Vector3 (Random.Range(0, 256), Random.Range(0, 256), Random.Range(0, 256));
+
+				//Creates a random RGB value to be the color associated with this path
+				//pathColor = new Vector3 (Random.Range (0, 256), Random.Range (0, 256), Random.Range (0, 256));
 			}
 
-			if (globalTileCount < 500) {
-				int hallLengthInTiles = Random.Range (3, 6);
-				int tilesCurrentlyInHall = 0;
+			//Determines how long the next hallway should be
+			int hallLengthInTiles = Random.Range (5, 7);
+			int tilesCurrentlyInHall = 0;
 
-				while (tilesCurrentlyInHall < hallLengthInTiles) {
+			//Starts a loop wherein a series of tiles, a.k.a. a "hall," are produced in a straight line 
+			while (tilesCurrentlyInHall < hallLengthInTiles
+				&& counter < 500) {
+					
+					//Creates a new floor tile
 					GameObject floorClone = (GameObject)Instantiate (floorPrefab, transform.position, Quaternion.Euler (0f, 0f, 0f));
 
 					//Gives it a groovy color
-					floorClone.GetComponent<Renderer> ().material.color = new Color (pathColor.x, pathColor.y, pathColor.z, 1);
+					floorClone.GetComponent<MeshRenderer> ().material.color = new Color (pathColor.x, pathColor.y, pathColor.z, 1);
 
+					//Moves the pathmaker sphere forward
 					transform.Translate (transform.forward * 5f);
-					globalTileCount++;		//Doesn't currently limit actual tile count in current version of code
-					Debug.Log (globalTileCount);
+
+					//Updates the number of tiles currently in the given hall
 					tilesCurrentlyInHall++;
-				}
-			} else {
-				GameObject.Destroy(this.gameObject);
+
+					counter++;
+					Debug.Log (counter);
 			}
 
-				counter++;
+		//Tells the pathmaker sphere to self-destruct, ensuring that it doesn't create any more paths
 		} else {
-			GameObject.Destroy(this.gameObject);
+			GameObject.Destroy (this.gameObject);
 		}
 	}
 
-} // end of class scope
+}
+ // end of class scope
 
 // MORE STEPS BELOW!!!........
 
@@ -96,7 +124,7 @@ public class Pathmaker : MonoBehaviour {
 //	IMPLEMENT AND TEST:
 //	- save your scene!!! the code could potentially be infinite / exponential, and crash Unity
 //	- put Pathmaker.cs on a sphere, configure all the prefabs in the Inspector, and test it to make sure it works
-//	STABILIZE: 
+//	STABILIZE:
 //	- code it so that all the Pathmakers can only spawn a grand total of 500 tiles in the entire world; how would you do that?
 //	- (hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code? if there are already enough tiles, then maybe the Pathmaker could Destroy my game object
 
@@ -136,11 +164,11 @@ public class Pathmaker : MonoBehaviour {
 // DYNAMIC CAMERA:
 // position the camera to center itself based on your generated world...
 // 1. keep a list of all your spawned tiles
-// 2. then calculate the average position of all of them (use a for() loop to go through the whole list) 
+// 2. then calculate the average position of all of them (use a for() loop to go through the whole list)
 // 3. then move your camera to that averaged center and make sure fieldOfView is wide enough?
 
 // BETTER UI:
-// learn how to use UI Sliders (https://unity3d.com/learn/tutorials/topics/user-interface-ui/ui-slider) 
+// learn how to use UI Sliders (https://unity3d.com/learn/tutorials/topics/user-interface-ui/ui-slider)
 // let us tweak various parameters and settings of our tech demo
 // let us click a UI Button to reload the scene, so we don't even need the keyboard anymore!
 
